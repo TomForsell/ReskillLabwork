9b6aad060a1b1e753d5c4284c8a3ba66c4fda9e7
Tom Forsell <77614564+TomForsell@users.noreply.github.com>
2021-01-26 13:50:45 +0100

    Commit after solving the leap year task

--- Student/StudentOpFlow/src/student/opflow/Program.java
+++ Student/StudentOpFlow/src/student/opflow/Program.java
@@ -2,10 +2,26 @@ package student.opflow;
 
 public class Program {
 
+
 	public static void main(String[] args) {
 
 		// Write your code here, to explore the use of operators and flow control.
+		findLeapYear();
+
+	}
 
 
+	private static void findLeapYear() {
+		//int day = 1;
+		//int month=1;
+
+		System.out.println("These are the years between 1900 and 2021 that are leap years: ");
+		for (int year=1900; year<=2021;year++ ){
+			//|| (year%4==0 && year%100!=0 )
+			if(year%400==0 || (year%4==0 && year%100!=0 )){
+				System.out.println("year: " +year);
+			}
+
+		}
 	}
 }
