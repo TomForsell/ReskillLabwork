e036ca58afe042d3ae82d045e44d4e35195ae202
Tom Forsell <77614564+TomForsell@users.noreply.github.com>
2021-01-26 16:17:17 +0100

    Commit and push after solving excersise 3. NO time to test which has to be done

--- Student/StudentOpFlow/src/student/opflow/Program.java
+++ Student/StudentOpFlow/src/student/opflow/Program.java
@@ -1,6 +1,7 @@
 package student.opflow;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Program {
 
@@ -8,12 +9,71 @@ public class Program {
 	public static void main(String[] args) {
 
 		// Write your code here, to explore the use of operators and flow control.
-		//findLeapYear();
-		validateYearAndDate();
-
+		findLeapYear(); // excersise 1
+		validateYearAndDate(); //excersise 2
+		//Excercise 3
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("input a year: ");
+		int year = scanner.nextInt();
+		System.out.print("\ninput a month: ");
+		int month = scanner.nextInt();
+		System.out.print("\nInput a date: ");
+		int date = scanner.nextInt();
+		if(isDateValid(year,date,month))
+			System.out.println("Date is Valid");
+		else
+			System.out.println("Date is Not Valid");
 	}
 
 
+	private static boolean isDateValid(int year, int date, int month){
+		//int date;
+		//int year;
+		//int month;
+		boolean isValid;
+
+			if(year<3000 && date!=0 && month!=0){
+				if (date<28){
+					//not so elegant solution,but got to test the continue
+					//System.out.println("date (day.month.year):" + date + "." + month + "." + year + " is Valid");
+					return true;
+					//go out of for next iteration in the for loop here without continuing
+				}
+				//check if dates are correct for months
+				switch (month){
+					case 2:
+						if (date<30 || (isLeapyear(year) && date<31) )
+							isValid = true;
+							//System.out.println("date (day.month.year):" + date + "." + month + "." + year + " is Valid");
+						else
+							isValid = false;
+							//System.out.println("date (day.month.year):" + date + "." + month + "." + year + " is NOT Valid");
+						break;
+					case 4:
+					case 6:
+					case 9:
+					case 11:
+						if(date<31)
+							isValid = true;
+							//System.out.println("date (day.month.year):" + date + "." + month + "." + year + " is Valid");
+						else
+							isValid = false;
+							//System.out.println("date (day.month.year):" + date + "." + month + "." + year + " is NOT Valid");
+						break;
+					default:
+						return true;
+						//System.out.println("date (day.month.year):" + date + "." + month + "." + year + " is Valid");
+
+				}
+
+			}
+			else{
+				return false;
+				//System.out.println("date (day.month.year):" + date + "." + month + "." + year + " is NOT valid");
+			}
+			return isValid;
+
+	}
 	private static void validateYearAndDate(){
 		int date;
 		int year;
